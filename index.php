7f79d8c7118e77dc85e0082d312b81efbb45a50b
Vitor Hugo <vitorhugo0031@outlook.com>
2023-03-19 16:44:50 -0300

    inicio

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+include('menu.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/index.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@400;500&family=Poppins:wght@500&display=swap" rel="stylesheet">
+    <title>index</title>
+</head>
+<body>
+   
+        <div class="foto">
+
+        </div>
+        <div class="redes">
+            <h3>instagram</h3>
+            <h3>facebook</h3>
+            <h3>whatsapp</h3>
+        </div>
+        <div class="quadras">
+            <img src="imgs/quadra" alt="">
+            <img src="imgs/quadra" alt="">
+            <img src="imgs/quadra" alt="">
+        </div>
+   
+    
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/menu.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>menu</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@400;500&family=Poppins:wght@500&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="menu">
+
+        <div class="esq">
+            <img src="imgs/logoofc.png" alt="" width="140px" heigth="120px">
+        </div>
+        <div class="cent">
+            <ul>
+                <li><a href="">Modalidades</a></li>
+                <li><a href="">Quem somos</a></li>
+                <li><a href="">Contato</a></li>
+                <li><a href="">Divulgue seu local</a></li>
+                
+            </ul>
+
+        </div>
+        <div class="dir">
+            <a href="">LOGIN</a>
+        </div>
+
+    </div>
+    
+</body>
+</html>
