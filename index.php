2efd4ad3eacd810061a837b9a614d85010303e76
Vitor Hugo <vitorhugo0031@outlook.com>
2023-03-20 16:18:29 -0300

    mexendo na tela inicial do site e colocando conteudo

--- index.php
+++ index.php
@@ -2,10 +2,8 @@
 include('menu.php');
 ?>
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="pt-br">
 <head>
-    <link rel="stylesheet" href="css/index.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,27 +11,21 @@ include('menu.php');
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@400;500&family=Poppins:wght@500&display=swap" rel="stylesheet">
-    <title>index</title>
+    <link rel="stylesheet" href="css/index.css">
+    <title>Principal</title>
 </head>
-
 <body>
-    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
-
+    <div class="carousel">
         <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-inner">
-                <div class="carousel-item active" data-bs-interval="2000">
-                    <img src="imgs/fotocerta.png" class="d-block w-100" height="100%" alt="">
+                <div class="carousel-item active" data-bs-interval="1900">
+                    <img src="imgs/fotocerta.png" class="d-block w-100" alt="">
                 </div>
-                <div class="carousel-item" data-bs-interval="2000">
-                    <img src="imgs/fotocerta2.png" class="d-block w-100" height="100%" alt="">
+                <div class="carousel-item" data-bs-interval="1900">
+                    <img src="imgs/fotocerta2.png" class="d-block w-100" alt="">
                 </div>
-                <div class="carousel-item" data-bs-interval="2000">
-                    <img src="imgs/3d22.png" class="d-block w-100" height="100%" alt="">
+                <div class="carousel-item" data-bs-interval="1900">
+                    <img src="imgs/3d22.png" class="d-block w-100" alt="">
                 </div>
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
@@ -45,18 +37,99 @@ include('menu.php');
                 <span class="visually-hidden">Next</span>
             </button>
         </div>
-
-        <div class="redes">
-            
+    </div>
+    <div class="content-prin">
+        <div class="img-content"></div>
+        <div class="text-locais">
+            <h1>Qual combina mais com você?</h1>
         </div>
-        <div class="quadras">
-            <img src="imgs/quadra" alt="">
-            <img src="imgs/quadra" alt="">
-            <img src="imgs/quadra" alt="">
+        <div class="locais">
+            <div class="campo">
+                <h3>Campo</h3>
+                <div class="box-campo" id="locais-hover"></div>
+            </div>
+            <div class="quadra">
+                <h3>Quadra</h3>
+                <div class="box-quadra" id="locais-hover"></div>
+            </div>
+            <div class="areia">
+                <h3>Areia</h3>
+                <div class="box-areia" id="locais-hover"></div>
+            </div>
+        </div>
+        <div class="cont-locais">
+            <div class="modalidades">
+                <a href="#">
+                    <h3>Pesquisar modalidades</h3>
+                </a>
+            </div>
         </div>
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
-        </script>
+        <div class="trabalhe-conosco"></div>
+
+        <div class="saopedro"></div>
+        <div class="saopedro-texto">
+            <div class="saopedro-texto1">
+                <h1>A.A. Jardim São Pedro</h1>
+            </div>
+            <div class="saopedro-texto2">
+                <h2>A grande equipe da Zona Leste da capital apoia <span>BusCampo</span></h2>
+            </div>
+            <div class="saopedro-texto3">
+                <div class="projeto">
+                    <ul>
+                        <li>Projetos Sociais</li>
+                        <li>Jogos Beneficientes</li>
+                        <li>Diversas categorias de futebol</li>
+                    </ul>
+                </div>
+                <div class="redes-sociais">
+                    <a href="https://www.instagram.com/jd.saopedro_oficial/">
+                        <p>Conheça o projeto</p>
+                    </a>
+                </div>
+            </div>
+            <div class="since-saopedro">
+                <p><i>Guaianazes - Zona Leste <span>Since 1968</span></i></p>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <div class="conteudo-footer">
+            <div class="boxPai-footer">
+                <div class="conteudo-footer-box1" id="box-footer">
+                    <img src="imgs/logoofc.png" alt="">
+                </div>
+                <div class="conteudo-footer-box2" id="box-footer">
+                    <p>Copyright 2023 ©</p>
+                </div>
+                <div class="conteudo-footer-box3" id="box-footer">
+                  <a href="#"><img src="imgs/seta-para-cima.png" alt=""></a>
+                </div>
+            </div>
+            <hr>
+            <div class="footer-servicos">
+                <div class="servicos-contato" id="footer-servicos">
+                   <a href="https://www.instagram.com/etecitaquera/"><p>@BusCampo</p></a>
+                    <p>(11) 2254-7627</p>
+                   <a href="https://www.google.com/maps/search/etec+de+itaquera/@-23.5404998,-46.5076225,12z/data=!3m1!4b1"><p>R. Virgínia Ferni, 400 - Itaquera, São Paulo - SP, 08253-000</p></a>
+                </div>
+                <div class="servicos-links" id="footer-servicos">
+                   <a href="#"><p>Contato</p></a>
+                   <a href="#"><p>Divulgue seu local</p></a>
+                   <a href="#"><p>Quem somos</p></a>
+                   <a href="#"><p>Política de privacidade</p></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
--- menu.php
+++ menu.php
@@ -5,25 +5,25 @@
     <link rel="stylesheet" href="css/menu.css">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>menu</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@400;500&family=Poppins:wght@500&display=swap" rel="stylesheet">
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-body-tertiary" id="container-0">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#"><img src="imgs/logoofc.png" alt="" heigth="150px" width="200px"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link" href="#">Features</a>
-        <a class="nav-link" href="#">Pricing</a>
-        <a class="nav-link disabled">Disabled</a>
+      <span class="quem-somos-span"><a class="nav-link " aria-current="page" href="#">Quem somos</a></span>
+        <a class="nav-link" href="#">Modalidades</a>
+        <a class="nav-link" href="#">Contato</a>
+        <a class="nav-link" href="#">Divulgue seu local</a>
+       <span class="login-span"><a class="nav-link" id="dif" href="#"> Login</a></span>
       </div>
     </div>
   </div>
