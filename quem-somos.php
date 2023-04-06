ccde3d0335c78c053834a6fe6004d427e3c61cbd
Vitor Hugo <vitorhugo0031@outlook.com>
2023-04-05 21:54:58 -0300

    mexendo na pg quem-somos

--- quem-somos.php
+++ quem-somos.php
@@ -13,12 +13,26 @@
 </head>
 
 <body>
-
-    <div class="text-quem-somos">
-        <h1>Quem-Somos</h1>
-    </div>
     <div class="texto-apresentacao">
-
+        <h1>
+            Bem-vindo à página Quem-Somos do nosso projeto de TCC! Somos uma equipe apaixonada e comprometida em realizar um trabalho de alta qualidade,
+            trazendo soluções inovadoras e criativas para o nosso tema.
+        </h1>
+        <div class="texto-p">
+        <p>
+            Nossa equipe é composta por 7 membros, todos com habilidades e formações distintas que, juntas, formam um time altamente capacitado e
+            diversificado.
+            <br>
+             A equipe se reúne regularmente para discutir ideias e trocar conhecimentos, sempre mantendo o foco no objetivo principal do projeto.
+            Cada membro tem uma função específica e trabalha em colaboração com os demais para atingir os resultados esperados.
+            <br>
+            Estamos entusiasmados em trabalhar neste projeto e esperamos que você possa acompanhar nossa jornada e se inspirar com nossas soluções criativas e inovadoras.
+             
+        </p>
+        </div>
+    </div>
+    <div class="membros">
+        <h1>Membros e habilidades</h1>
     </div>
     <div class="vitor">
         <div class="img-vitor">
@@ -49,6 +63,10 @@
         <div class="img-breno">
 
         </div>
+    </div>
+
+    <div class="footer-quem-somos">
+        <h3>Agradecemos sua visita à nossa página Quem-Somos</h3>
     </div>
 
 </body>
