22c8176944ca515a28498826c0663f0e587947ae
Gustavo Henrique <gustavo329886@gmail.com>
2023-04-25 02:10:04 -0300

    consertando pesquisar

--- pesquisa_by_categoria.act.php
+++ pesquisa_by_categoria.act.php
@@ -5,14 +5,15 @@
 
     require('connect.php');
         if(!$texto == ""){
-            $quadras = mysqli_query($con, "SELECT * FROM `tb_local` INNER JOIN `tb_categoria` ON `tb_local`.`id_categoria` = `tb_categoria`.`id_categoria` where `desc_categoria` like '%$texto%'");
+            $quadras = mysqli_query($con, "SELECT * FROM `tb_local` INNER JOIN `tb_categoria` ON `tb_local`.`id_categoria` = `tb_categoria`.`id_categoria` INNER JOIN `tb_estado` ON `tb_local`.`id_estado` = `tb_estado`.`id_estado` where `desc_categoria` like '%$texto%' order by `nome_local`");
 
             echo "<div class = \"box\">";
             while($quadra = mysqli_fetch_array($quadras)){
                 echo "<div class =\"sc\">";
                 echo "<p id= pag>$quadra[nome_local] </p>";
                 echo "<p id= pag>$quadra[desc_local] </p>";
-                echo "<p id= pag>$quadra[estado] </p>";
+                echo "<p id= pag>$quadra[desc_categoria] </p>";
+                echo "<p id= pag>$quadra[nome_estado] </p>";
                 echo "<p id= pagimg><img src=$quadra[img1]></p>";
                 // if(!isset($_SESSION['login']) || $_SESSION['login'] != true){
                     
