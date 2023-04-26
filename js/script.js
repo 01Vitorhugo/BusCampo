309f8b4bc51bf078a9b0fdf4141daeeff146a095
Vitor <vitorhugo0031@outlook.com>
2023-04-26 02:52:39 -0300

    js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,56 @@
 window.sr = ScrollReveal({ reset: true });
 
+
+// TEXTO LOCAIS
 sr.reveal('.text-locais', { 
-    distance: '0px',
-    delay: 1000,
-    duration: 900,
-    reset: true
+    delay: 200,
+    duration: 1000,
+    reset: true,
+    scale:  
+    0.5,
 });
+// FIM TEXTO LOCAIS
+
+// DIV LOCAIS
+sr.reveal('.campo', { 
+    delay: 300,
+    duration: 1000,
+    reset: true,
+    scale:  
+    0.5,
+});
+
+sr.reveal('.quadra', { 
+    delay: 300,
+    duration: 1000,
+    reset: true,
+    scale:  
+    0.5,
+});
+
+sr.reveal('.areia', { 
+    delay: 300,
+    duration: 1000,
+    reset: true,
+    scale:  
+    0.5,
+});
+// FIM DIV LOCAIS
+
+// CAROUSEL SUGESTAO-QUADRAS
+sr.reveal('.sugestao-content', { 
+    delay: 200,
+    duration: 1000,
+    reset: true,
+    scale:  
+    0.5,
+    
+});
+// FIM CAROUSEL SUGESTAO-QUADRAS
+
+// FOTO TRABALHE CONOSCO
 sr.reveal('.trabalhe-conosco', { 
-    distance: '0px',
-    delay: 400,
+    delay: 300,
     duration: 1000,
     reset: true,
     rotate: {
@@ -16,5 +58,85 @@ sr.reveal('.trabalhe-conosco', {
         y: 10
     }
 });
+// FIM FOTO TRABALHE CONOSCO
+
+// TEXTOS SAO PEDRO
+sr.reveal('.saopedro-texto1', { 
+    delay: 300,
+    duration: 1000,
+    reset: true,
+    scale:  
+        2,
+});
+
+sr.reveal('.saopedro-texto2', { 
+    delay: 400,
+    duration: 1000,
+    reset: true,
+    scale:  
+        2,
+});
+
+sr.reveal('.redes-sociais', { 
+    delay: 500,
+    duration: 1000,
+    reset: true,
+    scale:  
+        2,
+});
+
+// FIM TEXTOS SAO PEDRO
+
+// FOOTER
+sr.reveal('.conteudo-footer-box1', { 
+    delay: 200,
+    duration: 1000,
+    reset: true,
+    rotate:  {
+        x: 3,
+        y: 6,
+        z: 50
+    }
+});
+
+sr.reveal('.conteudo-footer-box2', { 
+    delay: 200,
+    duration: 1000,
+    reset: true,
+    scale:  
+        1.5,
+});
+
+sr.reveal('.conteudo-footer-box3', { 
+    delay: 200,
+    duration: 1000,
+    reset: true,
+    rotate:  {
+        x: 3,
+        y: 6,
+        z: 50
+    }
+});
+
+sr.reveal('.footer-servicos', { 
+    delay: 200,
+    duration: 1000,
+    reset: true,
+    scale:  
+        0.5,
+});
+
+sr.reveal('.servicos-links', { 
+    delay: 200,
+    duration: 1000,
+    reset: true,
+    scale:  
+        0.5,
+});
 
 
+
+
+
+
+// FIM FOOTER
