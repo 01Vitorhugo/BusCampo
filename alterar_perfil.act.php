4a823e821ccddd8c16f14105bbf3cc26c326eda7
Gustavo Henrique <gustavo329886@gmail.com>
2023-05-01 23:25:06 -0300

    mais bugs

--- alterar_perfil.act.php
+++ alterar_perfil.act.php
@@ -3,8 +3,32 @@
     require('connect.php');
     extract($_POST);
 
-    $old_senha = md5($old_senha);
-    $new_senha = md5($new_senha);
+    if($old_senha != ""){
+        $old_senha = md5($old_senha);
+    }
+    if($new_senha != ""){
+        $new_senha = md5($new_senha);
+    }
+
+    if($old_senha == ""){
+        if($_SESSION['tipo'] == true){
+            if(mysqli_query($con, "UPDATE `tb_locador` SET `nome_cliente` = '$nome_cliente', `email` = '$email', `telefone` = '$tel', `doc` = '$doc' WHERE `tb_locador`.`id_cliente` = '$id_cliente';")){
+                $msg = "Alterado com sucesso";
+                header("location:perfil.php");   
+              }else{
+                $msg = "Erro na alteração";
+                header("location:alterar_perfil.php");   
+              }
+        }else if($_SESSION['tipo'] == false){
+            if(mysqli_query($con, "UPDATE `tb_locatario` SET `nome_cliente` = '$nome_cliente', `email` = '$email' WHERE `tb_locatario`.`id_cliente` = '$id_cliente';")){
+                $msg = "Alterado com sucesso";
+                header("location:perfil.php");   
+              }else{
+                $msg = "Erro na alteração";
+                header("location:alterar_perfil.php");   
+              }
+        }
+    }
 
     if($_SESSION['tipo'] == true){
         $query_senha = mysqli_query($con, "SELECT senha FROM `tb_locador` WHERE `tb_locador`.`id_cliente`;");
@@ -14,7 +38,7 @@
         $senha_atual = mysqli_fetch_array($query_senha);
     }
 
-    if($senha_atual['senha'] == $old_senha){
+    if($senha_atual['senha'] == $old_senha && $new_senha != ""){
         if($_SESSION['tipo'] == true){
             if(mysqli_query($con, "UPDATE `tb_locador` SET `nome_cliente` = '$nome_cliente', `email` = '$email', `telefone` = '$tel', `doc` = '$doc', `senha` = '$new_senha' WHERE `tb_locador`.`id_cliente` = '$id_cliente';")){
                 $msg = "Alterado com sucesso";
