cd8516a44587b3dc67bb3fb719d2fdf42d3f606c
Cristhian Andrade de novaes <cristhian.ctn@hotmail.com>
2023-06-05 00:06:49 -0300

    ajustes de tela

--- quem-somos.php
+++ quem-somos.php
@@ -40,9 +40,11 @@
         <a href="">
         <img src="imgs/instagram.png" alt="">
         </a>
-        
       </div>
 
+      <div class="mapa-local">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d14630.782638318427!2d-46.46316490698675!3d-23.54344620957685!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1setec%20de%20itaquera!5e0!3m2!1spt-BR!2sbr!4v1685933707430!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
 </body>
 
 <script src="js/script-quem-somos.js"></script>
